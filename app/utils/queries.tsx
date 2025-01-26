bef397ee6919da161e779918a51fa7d2a3905856
berryintern <info@usestrawberry.ai>
2025-01-26 09:53:43 +0400

    no cache

--- app/utils/queries.tsx
+++ app/utils/queries.tsx
@@ -2,7 +2,7 @@ import prisma from "@/lib/prisma";
 
 export async function countAllTickers() {
   // @ts-ignore
-  const result = await prisma.twitter_sentiment.aggregateRaw({
+  const result = (await prisma.twitter_sentiment.aggregateRaw({
     pipeline: [
       { $unwind: "$narratives" },
       { $unwind: "$narratives.detectedTickers" },
@@ -13,7 +13,7 @@ export async function countAllTickers() {
         },
       },
     ],
-  });
+  })) as { totalTickers: number }[];
   const totalTickers = result[0]?.totalTickers || 0;
   return totalTickers;
 }
@@ -30,7 +30,10 @@ export async function getPostsCount() {
 
 export async function getNarratives() {
   const response = await fetch(
-    "https://luigi-bigdata.usestrawberry.ai/narratives"
+    "https://luigi-bigdata.usestrawberry.ai/narratives",
+    {
+      cache: "no-store",
+    }
   );
   return response.json();
 }
@@ -57,14 +60,20 @@ export async function getAllJobsThoughts() {
 
 export async function getTickers() {
   const response = await fetch(
-    "https://luigi-bigdata.usestrawberry.ai/tickers?hours=18&limit=100"
+    "https://luigi-bigdata.usestrawberry.ai/tickers?hours=18&limit=100",
+    {
+      cache: "no-store",
+    }
   );
   return response.json();
 }
 
 export async function getLastJob() {
   const response = await fetch(
-    "https://luigi-reasoning.usestrawberry.ai/last_job"
+    "https://luigi-reasoning.usestrawberry.ai/last_job",
+    {
+      cache: "no-store",
+    }
   );
   return response.json();
 }
